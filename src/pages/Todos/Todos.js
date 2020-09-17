840e096a50815386ac1366dce4f995c6c16d4ed7
azezmohammed <muhamadaziz047@gmail.com>
2020-09-17 23:15:37 +0700

    add todo and Uppercase

--- src/pages/Todos/Todos.js
+++ src/pages/Todos/Todos.js
@@ -1,11 +1,101 @@
 import React from "react";
+import "./Todos.css";
+// import ListItems from "./ListItems";
 
-function Todos() {
-  return (
-    <div>
-      <h1>Ini adalah halaman untuk Todos</h1>
-    </div>
-  );
+class Todos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.State = {
+      items: [],
+      hurufTerbaru: {
+        text: "",
+        key: "",
+      },
+    };
+    this.tambahkanHuruf = this.tambahkanHuruf.bind(this);
+    this.menanganiHuruf = this.menanganiHuruf.bind(this);
+    // this.deleteItem = this.deleteItem.bind(this);
+    // this.setUpdate = this.setUpdate.bind(this);
+  }
+  // Tambahkan Item
+  tambahkanHuruf = (event) => {
+    event.preventDefault();
+    const itemTerbaru = this.state.hurufTerbaru;
+    console.log(itemTerbaru);
+    const items = [...this.state.items, itemTerbaru];
+    this.setState({
+      items: items,
+      hurufTerbaru: {
+        text: "",
+        key: "",
+      },
+    });
+    // if (itemTerbaru.text !== "") {
+    //   const items = [...this.state.items, itemTerbaru];
+    //   this.setState({
+    //     items: items,
+    //     hurufTerbaru: {
+    //       text: "",
+    //       key: "",
+    //     },
+    //   });
+    // }
+  };
+  // Input Item
+  menanganiHuruf = (event) => {
+    this.setState({
+      hurufTerbaru: {
+        text: event.target.value,
+        key: Date.now(),
+      },
+    });
+    console.log("hai menangani hurufnya bisa");
+  };
+  // Hapus Item
+  // deleteItem = (key) => {
+  //   const filteredItems = this.state.items.filter((item) => item.key !== key);
+  //   this.setState({
+  //     items: filteredItems,
+  //   });
+  // };
+  // Update Item
+  // setUpdate = (text, key) => {
+  //   console.log("items:" + this.state.items);
+  //   const items = this.state.items;
+  //   items.map((item) => {
+  //     if (item.key === key) {
+  //       console.log(item.key + "    " + key);
+  //       item.text = text;
+  //     }
+  //   });
+  //   this.setState({
+  //     items: items,
+  //   });
+  // };
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1 className="judul">Masukkan To Do List Anda</h1>
+          <form id="to-do-form" onSubmit={this.tambahkanHuruf}>
+            <input
+              type="text"
+              placeholder="Masukkan Kegiatan"
+              // value={this.state.hurufTerbaru.text}
+              onChange={this.menanganiHuruf}
+            ></input>
+            <button type="submit">Add</button>
+          </form>
+          {/* <p>{this.state.items.text}</p> */}
+          {/* <ListItems
+            items={this.state.items}
+            deleteItem={this.deleteItem}
+            setUpdate={this.setUpdate}
+          /> */}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Todos;
--- src/pages/Uppercase/Uppercase.js
+++ src/pages/Uppercase/Uppercase.js
@@ -1,11 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
+import { MDBBtn } from "mdbreact";
+import "./Uppercase.css";
 
-function Home() {
-  return (
-    <div>
-      <h1>Ini adalah halaman Uppercase</h1>
-    </div>
-  );
+export class Uppercase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "muhamad aziz",
+    };
+  }
+
+  memperbesarkan = (string) => {
+    prompt("masukkan string anda");
+    alert(string.toUpperCase());
+  };
+
+  memperkecilakan = (huruf) => {
+    prompt("masukkan string anda");
+    alert(huruf.toLowerCase());
+  };
+
+  //   method untuk mengecilkan
+  // toLowerCase = () => {
+  //   const lowerCase = this.state.text.toLowerCase();
+  //   this.setState({
+  //     text: lowerCase,
+  //   });
+  // };
+
+  //   method untuk memperbesar
+  // toUpperCase = () => {
+  //   const upperCase = this.state.text.toUpperCase();
+  //   this.setState({
+  //     text: upperCase,
+  //   });
+  // };
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Masukkan teks anda</h1>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon">
+                <i className="fas fa-pencil-alt prefix"></i>
+              </span>
+            </div>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="5"
+            ></textarea>
+          </div>
+        </div>
+        <h1>{this.state.text}</h1>
+        <MDBBtn
+          onClick={() => {
+            this.memperbesarkan();
+          }}
+          color="secondary"
+        >
+          Perbesar
+        </MDBBtn>
+        <MDBBtn
+          onClick={() => {
+            this.memperkecilakan();
+          }}
+          color="success"
+        >
+          perkecil
+        </MDBBtn>
+      </div>
+    );
+  }
 }
 
-export default Home;
+export default Uppercase;
