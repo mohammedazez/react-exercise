7bb753faddd8dd2150b858544fd1c5aa50a586c5
azezmohammed <muhamadaziz047@gmail.com>
2020-09-20 17:02:30 +0700

    update uppercase

--- src/pages/Uppercase/Uppercase.js
+++ src/pages/Uppercase/Uppercase.js
@@ -1,78 +1,36 @@
-import React, { Component } from "react";
-import { MDBBtn } from "mdbreact";
+import React, { useState } from "react";
 import "./Uppercase.css";
 
-export class Uppercase extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "muhamad aziz",
-    };
-  }
+function Uppercase() {
+  const [masukanTeks, setMasukanTeks] = useState("");
+  const [hasil, setHasil] = useState("");
 
-  memperbesarkan = (string) => {
-    prompt("masukkan string anda");
-    alert(string.toUpperCase());
+  let handleChange = (event) => {
+    let inputan = event.target.value;
+    setMasukanTeks(inputan);
+    let hasilTeks = inputan.toUpperCase();
+    setHasil(hasilTeks);
   };
-
-  memperkecilakan = (huruf) => {
-    prompt("masukkan string anda");
-    alert(huruf.toLowerCase());
-  };
-
-  //   method untuk mengecilkan
-  // toLowerCase = () => {
-  //   const lowerCase = this.state.text.toLowerCase();
-  //   this.setState({
-  //     text: lowerCase,
-  //   });
-  // };
-
-  //   method untuk memperbesar
-  // toUpperCase = () => {
-  //   const upperCase = this.state.text.toUpperCase();
-  //   this.setState({
-  //     text: upperCase,
-  //   });
-  // };
-  render() {
-    return (
-      <div>
-        <div>
-          <h1>Masukkan teks anda</h1>
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon">
-                <i className="fas fa-pencil-alt prefix"></i>
-              </span>
-            </div>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="5"
-            ></textarea>
-          </div>
+  return (
+    <div>
+      <h1>Masukkan teks anda</h1>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon">
+            <i className="fas fa-pencil-alt prefix"></i>
+          </span>
         </div>
-        <h1>{this.state.text}</h1>
-        <MDBBtn
-          onClick={() => {
-            this.memperbesarkan();
-          }}
-          color="secondary"
-        >
-          Perbesar
-        </MDBBtn>
-        <MDBBtn
-          onClick={() => {
-            this.memperkecilakan();
-          }}
-          color="success"
-        >
-          perkecil
-        </MDBBtn>
+        <textarea
+          onChange={handleChange}
+          value={masukanTeks}
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="5"
+        ></textarea>
       </div>
-    );
-  }
+      <h1>{hasil}</h1>
+    </div>
+  );
 }
 
 export default Uppercase;
